2c5e8d2b39580417a2530aa34a40aba9c6ca9f37
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-03-03 18:38:48 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,per;     scanf("%d",&n);     for(i=1;i<=sqrt(n);i++)     {     per=i*i;     }     if(per==n)     {         printf("True");     }
        else
        {
            printf("False");
        }
        
    }

---
+++ Square_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,per;
+    scanf("%d",&n);
+    for(i=1;i<=sqrt(n);i++)
+    {
+    per=i*i;
+    }
+    if(per==n)
+    {
+        printf("True");
+    }
+    
+    else
+    {
+        printf("False");
+    }
+    
+}
