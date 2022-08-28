d1065914a2982d8749c160f512641e7ecbb1802b
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-08-28 07:38:34 +0530

    #include<stdio.h> int main() {     int n,i,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             if(i%2==0)             {                 printf("False");                 return 0;             }         }     }     printf("True");     return 0; }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,25 +1,23 @@
 #include<stdio.h>
- int main()
- {
-     int n,i;
-     scanf("%d",&n);
-     int arr[n];
-     for(i=0;i<n;i++)
-     {
-         scanf("%d",&arr[i]);
-     }
-     for(i=0;i<n;i++)
-     {
-         if(arr[i]%2!=0)
-         {
-             if(i%2==0)
-         {
-             printf("False");
-             return 0;
-         }
-         }
-     }
-     
-     printf("True");
-     return 0;
- }
+int main()
+{
+    int n,i,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            if(i%2==0)
+            {
+                printf("False");
+                return 0;
+            }
+        }
+    }
+    printf("True");
+    return 0;
+}
